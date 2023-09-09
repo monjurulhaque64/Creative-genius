21da4f8158a487f754e1e58d70ebccf760a2e193
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-09-09 12:27:58 +0600

    update bannner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -33,12 +33,12 @@ const Banner = () => {
         modules={[Autoplay]}
         className="mySwiper"
       >
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <div className="w-full h-[800px]  bannerimg1">
             <div className="lg:ml-[100px] ml-8  lg:w-[726px] w-[350px] lg:pt-[200px] pt-[50px] ">
               <h1 className="text-[32px] md:text-[64px] font-[Saira Stencil One] font-bold text-white">
                 Plan Your Day With Creative{" "}
-                <span className="text-[#ff0000]">Campus</span>
+                <span className="text-[#3C4FF8]">Campus</span>
               </h1>
               <p className="font-[Poppins] text-[17px] text-white text-justify font-medium mt-2">
                 Welcome to creative campus. At Creative Campus, we believe in
@@ -46,7 +46,7 @@ const Banner = () => {
                 professionals are dedicated to providing exceptional service
                 according to your needs.
               </p>
-              <button className="btn btn-info bg-[#ff0000] hover:bg-[#f35454] border-[#ff0000] mt-[46px] lg:mr-[100px] text-white">
+              <button className="btn download-app mt-[46px] lg:mr-[100px] text-white">
                 Download App{" "}
                 <span>
                   <FaGooglePlay></FaGooglePlay>
@@ -56,42 +56,48 @@ const Banner = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="circle bg-[#FF9324] left-[calc(-80px)] top-[calc(280px)] transform absolute ml-[1150px] "></div>
+          <div className="circle bg-[#FF9324] left-[calc(-80px)] top-[calc(280px)] transform absolute ml-[1150px] "></div>
           <div className="w-full h-[800px] bannerimg2 relative">
-          <div className="centeredDiv"></div>
+            <div className="centeredDiv"></div>
             <div className="flex justify-between">
-              <div className="lg:ml-[100px] ml-8  lg:w-[726px] w-[350px] lg:pt-[250px] pt-[50px] ">
+              <div className="lg:ml-[50px] ml-8  lg:w-[726px] w-[350px] lg:pt-[250px] pt-[50px] ">
                 <h1 className="lg:text-[64px] text-[34px] font-[Saira Stencil One] font-bold text-white">
                   Learning <span className="titleBg">Campus</span>
                 </h1>
                 <p className="font-[Poppins] text-[17px] text-white text-justify font-medium mt-2">
-                The future of digital learning technology in teaching and learning.
+                  The future of digital learning technology in teaching and learning.
                 </p>
-                <button className="btn btn-info bg-[#ff0000] hover:bg-[#f35454] border-[#ff0000] mt-[46px] lg:mr-[100px] text-white">
+                <button className="btn download-app mt-[46px] lg:mr-[100px] text-white">
                   Download App{" "}
                   <span>
                     <FaGooglePlay></FaGooglePlay>
                   </span>
                 </button>
               </div>
-              <div className="flex lg:mr-[250px] mr-[100px] lg:pt-[192px] pt-[102px] lg:gap-12 ">
-                <img src={iphone12Dark} alt="" />
+              <div className="flex lg:mr-[150px] mr-[100px] lg:pt-[192px] pt-[102px] lg:gap-12 relative">
+                <div className="absolute md:top-[18rem] md:-ml-[4.5rem]"><svg xmlns="http://www.w3.org/2000/svg" width="147" height="147" viewBox="0 0 147 147" fill="none">
+                  <circle cx="73.5" cy="73.5" r="73.5" fill="#FF9324" />
+                </svg></div>
+                <img className="z-10" src={iphone12Dark} alt="" />
                 <img src={iphone12Lignt} alt="" />
               </div>
             </div>
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
         <SwiperSlide>
           <div className="w-full h-[800px] bannerimg3 relative">
-            <div className="flex justify-between">
-              <div className="lg:ml-[100px] ml-8  lg:w-[726px] w-[350px] lg:pt-[250px] pt-[50px] ">
-                <h1 className="lg:text-[64px] text-[34px] font-[Saira Stencil One] font-[700] text-white">
-                  Learning Camp<span className="titleBgCircle text-white">us</span>
+            <div className="md:flex md:justify-between">
+              <div className="lg:ml-[100px] ml-8  lg:w-[726px] w-[350px] lg:pt-[250px] pt-[50px]">
+                <div className="absolute svg-logo top-[17rem] right-[41rem] -z-[10] "><svg className="svg" xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 80 80" fill="none">
+                  <circle cx="40" cy="40" r="40" fill="#4756DC" />
+                </svg></div>
+                <h1 className="lg:text-[64px] text-[34px] font-[Saira Stencil One] font-[700] text-white z-10 ">
+                  Learning Campus
                 </h1>
                 <p className="font-[Poppins] text-[17px] text-white text-justify font-medium mt-2">
-                The future academic platform for teaching and learning
+                  The future academic platform for teaching and learning
                 </p>
-                <div className="flex">
+                <div className="flex  ">
                   <a className="w-[153px] h-[41px]" href="">
                     <img src={googleBtn} alt="" />
                   </a>
@@ -100,14 +106,20 @@ const Banner = () => {
                   </a>
                 </div>
               </div>
-              <div className="flex lg:mr-[250px] mr-[100px] lg:pt-[192px] pt-[102px]">
-                <img className="absolute -ml-44" src={slider3ImgChart} alt="" />
-                <img className="relative mb-36" src={slider3ImgLogo} alt="" />
+              {/* <div className="flex lg:mr-[25px] right-side-banner lg:pt-[192px] pt-[102px]">
+                <img className="md:absolute md:-ml-44 salary-img" src={slider3ImgChart} alt="" />
+                <img className="md:relative md:mb-36 learning-img" src={slider3ImgLogo} alt="" />
+              </div> */}
+              <div className="right-side mt-20">
+                <div className="flex right-side-banner ">
+                  <img className="relative md:left-[-16rem] salary-img" src={slider3ImgChart} alt="" />
+                  <img className="absolute md:right-[2rem] md:top-[2rem] learning-img" src={slider3ImgLogo} alt="" />
+                </div>
               </div>
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <div className="w-full h-[800px] bannerimg4 relative">
             <div className="flex justify-between">
               <div className="lg:ml-[100px] ml-8  lg:w-[726px] w-[350px] lg:pt-[250px] pt-[50px] ">
@@ -115,7 +127,7 @@ const Banner = () => {
                   Learning Campus
                 </h1>
                 <p className="font-[Poppins] text-[17px] text-white text-justify font-medium mt-2">
-                The future of digital academic platforms.
+                  The future of digital academic platforms.
                 </p>
                 <button className="btn btn-info bg-[#ff0000] hover:bg-[#f35454] border-[#ff0000] mt-[46px] lg:mr-[100px] text-white">
                   Download App{" "}
@@ -133,7 +145,7 @@ const Banner = () => {
               </div>
             </div>
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
       </Swiper>
       <div className="absolute inset-x-0 mx-auto top-[837px]">
         <ClientSpring></ClientSpring>
