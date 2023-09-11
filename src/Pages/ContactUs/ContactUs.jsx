84ada1ac953b13fa3f7a778cf2d6497d7c020f02
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-09-11 10:37:53 +0600

    black-222

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -46,10 +46,10 @@ const ContactUs = () => {
         <title>Creative Campus || Contact Us</title>
       </Helmet>
       <div className="mx-auto text-center h-[130px] md:h-[200px]">
-        <h1 className="heading font-[600] text-[28px] md:text-[48px] text-white">
+        <h1 className="heading font-[Poppins] font-[600] text-[28px] md:text-[48px] text-white">
           Contact Us
         </h1>
-        <p className="heading font-[400] w-[300px] md:w-[650px] text-center mx-auto text-[14px] md:text-[18px] text-white md:mt-[20px]">
+        <p className="heading font-[400] font-[Poppins] w-[300px] md:w-[650px] text-center mx-auto text-[16px] text-white md:mt-[20px]">
           Here's our contact details so visitors can get in touch with us on
           their first attempt.
         </p>
@@ -58,26 +58,26 @@ const ContactUs = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 ">
         <div className="lg:mt-[255px] ml-4 lg:ml-0">
         <div className="contuct-circle-left"></div>
-          <h1 className="heading font-[600] text-[20px] md:text-[28px] text-white mb-[22px]">
+          <h1 className="heading font-[600] font-[Poppins] text-[20px] md:text-[30px] text-white mb-[22px]">
             Get In Touch
           </h1>
-          <p className="heading  font-[400] text-[14px] md:text-[18px] text-white mb-8 md:mb-[70px]">
+          <p className="heading  font-[400] font-[Poppins] text-[14px] md:text-[20px] text-white mb-8 md:mb-[70px]">
             We're eager to hear from you! Feel free to reach out for inquiries,
             collaborations, or just to say hello.
           </p>
-          <h3 className="heading w-[271px] font-[500]  text-white flex mb-5 md:mb-[30px]">
+          <h3 className="heading w-[271px] font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white flex mb-5 md:mb-[31px]">
             <span className="mr-2">
               <MdLocationOn size={30}></MdLocationOn>
             </span>
             Mirpur, Dhaka, Bangladesh
           </h3>
-          <h3 className="heading w-[271px] font-[500] text-[14px] md:text-[18px] text-white flex mb-5 md:mb-[30px]">
+          <h3 className="heading w-[271px] font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white flex mb-5 md:mb-[30px]">
             <span className="mr-2">
               <MdPhone size={30}></MdPhone>
             </span>
             +880123456789
           </h3>
-          <h3 className="heading w-[271px] font-[500] text-[14px] md:text-[18px] text-white flex  mb-8 md:mb-[30px]">
+          <h3 className="heading w-[271px] font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white flex  mb-8 md:mb-[30px]">
             <span className="mr-2">
               <MdEmail size={30}></MdEmail>
             </span>
@@ -87,7 +87,7 @@ const ContactUs = () => {
         <div className="form-container contect-container mt-[106px]">
           <form ref={formRef} onSubmit={handleSubmit}>
             <div className="form-group">
-              <label htmlFor="name">Full Name</label>
+              <label htmlFor="name" className="font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white">Full Name</label>
               <input
                 type="text"
                 id="name"
@@ -95,23 +95,23 @@ const ContactUs = () => {
                 value={formData.name}
                 onChange={handleChange}
                 placeholder="Full Name"
-                className="!text-white"
+                className="!text-[#A0A0A0] !font-[500] !font-[Poppins] !text-[14px] !md:text-[16px] !hover:border-[#A0A0A0]"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email" className="font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white">Email</label>
               <input
                 type="email"
                 id="email"
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
-                className="!text-white"
+                className="!text-[#A0A0A0] !font-[500] !font-[Poppins] !text-[14px] !md:text-[16px]"
                 placeholder="Enter Email Address"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="reason">Reason</label>
+              <label htmlFor="reason" className="font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white">Reason</label>
               <input
                 type="text"
                 id="reason"
@@ -119,11 +119,11 @@ const ContactUs = () => {
                 value={formData.reason}
                 onChange={handleChange}
                 placeholder="Reason"
-                className="!text-white"
+                className="!text-[#A0A0A0] !font-[500] !font-[Poppins] !text-[14px] !md:text-[16px]"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="message">Message</label>
+              <label htmlFor="message" className="font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white">Message</label>
               <textarea
                 id="message"
                 name="message"
@@ -131,10 +131,10 @@ const ContactUs = () => {
                 onChange={handleChange}
                 rows="5"
                 placeholder="Text Your Message"
-                className="!text-white"
+                className="!text-[#A0A0A0] !font-[500] !font-[Poppins] !text-[14px] !md:text-[16px]"
               />
             </div>
-            <button className="contect-button md:text-[20px] text-[14px] " type="submit">
+            <button className="contect-button font-[500] font-[Poppins] text-[14px] md:text-[20px] text-white " type="submit">
               Submit Now
             </button>
           </form>
